75e8665651ad1441da47ee452040de9759ae5d0d
BVSRCR7 <76527390+BVSRCR7@users.noreply.github.com>
2021-09-15 20:52:47 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX=0;
+noseY=0;
+difference=0;
+rightWristX=0;
+leftWristX=0;
 function setup(){
     video = createCapture(VIDEO);
     video.size(550,500);
@@ -14,7 +19,12 @@ function modelLoaded(){
 }
 
 function draw(){
-    background('#969A97')
+
+    document.getElementById("square_sides").innerHTML = "width and height of the square will be = " + difference +"px";
+    background('#969A97');
+    fill('#85CED1');
+    stroke('#85CED1');
+    square(noseX,noseY,difference);
 }
 
 function gotPoses(results)
@@ -22,5 +32,13 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + "noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWrist = " + leftWristX + "rightWristX = " + rightWristX + "difference = " + difference);
     }
 }
